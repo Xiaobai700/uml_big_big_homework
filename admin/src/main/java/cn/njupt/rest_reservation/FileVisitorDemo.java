30b1132a010a66bd8f6e83902b5923a097aaedd1
zhangqiao <zhangqiao@njbandou.com>
2019-04-08 11:02:07 +0800

    NIO2demo

--- admin/src/main/java/cn/njupt/rest_reservation/FileVisitorDemo.java
+++ admin/src/main/java/cn/njupt/rest_reservation/FileVisitorDemo.java
@@ -1,7 +1,35 @@
 package cn.njupt.rest_reservation;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zhangqiao on 2019/4/7.
  */
-public class FileVisitorDemo {
+public class FileVisitorDemo{
+    public static  void main(String[] args){
+        List<String> paths = new ArrayList<>();
+        paths = getAllFilePaths(new File("c:\\"),paths);
+        for (String path:paths) {
+            System.out.println(path);
+        }
+
+    }
+    private static List<String> getAllFilePaths(File filePath,List<String> filePaths){
+        File[] files = filePath.listFiles();
+        if(files == null){
+            return  filePaths;
+        }
+        for (File f:files){
+            if(f.isDirectory()){
+                filePaths.add(f.getPath());
+                getAllFilePaths(f,filePaths);
+
+            }else {
+                filePaths.add(f.getPath());
+            }
+        }
+        return  filePaths;
+    }
 }
--- admin/src/main/java/cn/njupt/rest_reservation/NIO2Demo.java
+++ admin/src/main/java/cn/njupt/rest_reservation/NIO2Demo.java
@@ -1,7 +1,31 @@
 package cn.njupt.rest_reservation;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zhangqiao on 2019/4/7.
  */
 public class NIO2Demo {
+    public  static void main(String[] args)throws IOException{
+        try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(args[0]))){
+            List<String> files = new ArrayList<>();
+            for (Path path:directoryStream) {
+                if(Files.isDirectory(path)){
+                    System.out.printf("[%s]%n",path.getFileName());
+                }else {
+                    files.add(path.getFileName().toString());
+                }
+            }
+            for(int i = 0; i < files.size();i++){
+                System.out.println(files.get(i).toString());
+            }
+        }
+        /*Iterable<Path> dirs = FileSystems.getDefault().getRootDirectories();
+        for (Path dir:dirs) {
+            System.out.println(dir.getRoot());
+        }*/
+    }
 }
